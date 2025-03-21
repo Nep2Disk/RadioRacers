67d62b6ee4d9fd898b403b9b4c936acabe1ef31d
blonded <iguessitsfuckme@proton.me>
2025-03-21 12:46:43 +0000

    Bump max emote height with text by 1 + play sounds during emote menus + fix bug in V_RR_ScaledWordWrap

--- src/hu_stuff.c
+++ src/hu_stuff.c
@@ -1132,6 +1132,10 @@ void HU_Ticker(void)
 	}
 
 	resynch_ticker++;
+
+	// RADIO
+	if (hu_radio_tick > 0)
+		hu_radio_tick--;
 }
 
 static boolean teamtalk = false;
@@ -1249,8 +1253,9 @@ static boolean RR_HU_Responder(INT32 c)
 				RR_CheckChatEnterforEmoteMenu();
 				return true;
 			}
-			if(is_emote_preview_on) {
-				RR_ResetEmoteSearchQuery();
+			if (is_emote_preview_on && !is_emote_menu_on) {
+				RR_SelectEmoteFromPreview();
+				return true;
 			}
 			HU_sendChatMessage();
 		}
