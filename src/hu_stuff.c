27392e974e1c72ba3370888f8d73a16ac9065084
blonded <iguessitsfuckme@proton.me>
2025-05-23 18:05:44 +0100

    Change emote menu UX + add missing ANIMDEFS for custom tripwire

--- src/hu_stuff.c
+++ src/hu_stuff.c
@@ -1261,7 +1261,7 @@ static boolean RR_HU_Responder(INT32 c)
 		if (!CHAT_MUTE)
 		{
 			if(is_emote_menu_on) {
-				RR_CheckChatEnterforEmoteMenu();
+				// Don't close the chatbox, wait until the player closes it
 				return true;
 			}
 			if(is_emote_preview_on) {
@@ -1279,6 +1279,11 @@ static boolean RR_HU_Responder(INT32 c)
 	} else if ((c == 'f' || c == 'F') && ctrldown) { // Favouriting
 		if (is_emote_menu_on) {
 			RR_UpdateFavouriteEmotes();
+			return true;
+		}
+	} else if(c == '=' && ctrldown) { // Sorting
+		if (!is_emote_preview_on && is_emote_menu_on) {
+			CV_AddValue(&cv_chat_emotes_sort, 1);
 			return true;
 		}
 	} else if (c == KEY_ESCAPE
@@ -1360,8 +1365,8 @@ static boolean RR_HU_Responder(INT32 c)
 			RR_SelectEmoteFromPreview();
 			return true;
 		}
-		if (!is_emote_preview_on && is_emote_menu_on) {
-			CV_AddValue(&cv_chat_emotes_sort, 1);
+		if(!is_emote_preview_on && is_emote_menu_on) {
+			RR_CheckChatEnterforEmoteMenu();
 			return true;
 		}
 	}
