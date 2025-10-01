f3ff1e163dcacb4d57b9c8fce80485c7565123e0
blonded <iguessitsfuckme@proton.me>
2025-10-01 20:01:21 +0100

    Move hudfeed condition for player damage

--- src/p_inter.c
+++ src/p_inter.c
@@ -3587,11 +3587,6 @@ static boolean P_DamageMobjCompat(mobj_t *target, mobj_t *inflictor, mobj_t *sou
 			}
 
 			K_DefensiveOverdrive(target->player);
-
-			// RadioRacers: .. right around here
-			if (inflictor) {
-				RR_PushPlayerDamageToFeed(source, target, inflictor);
-			}
 		}
 	}
 	else
@@ -4441,6 +4436,11 @@ boolean P_DamageMobj(mobj_t *target, mobj_t *inflictor, mobj_t *source, INT32 da
 			}
 
 			K_DefensiveOverdrive(target->player);
+
+			// RadioRacers: .. right around here
+			if (inflictor) {
+				RR_PushPlayerDamageToFeed(source, target, inflictor);
+			}
 		}
 	}
 	else
